f9d3d9b53af882254744718e7db8e4845e9ffb8c
hammsik <asbbrp@gmail.com>
2023-07-11 02:58:20 +0900

    question class 생성, 퀴즈 페이지 버튼 리스트로 수정

--- lib/layout_question.dart
+++ lib/layout_question.dart
@@ -12,6 +12,28 @@ class QuestionPage extends StatefulWidget {
 class _QuestionPageState extends State<QuestionPage> {
   @override
   Widget build(BuildContext context) {
-    return const Text('QuestionPage');
+    return Column(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: [
+        const Text('The question...'),
+        const SizedBox(height: 30),
+        ElevatedButton(
+          onPressed: () {},
+          child: const Text('Answer 1'),
+        ),
+        ElevatedButton(
+          onPressed: () {},
+          child: const Text('Answer 2'),
+        ),
+        ElevatedButton(
+          onPressed: () {},
+          child: const Text('Answer 3'),
+        ),
+        ElevatedButton(
+          onPressed: () {},
+          child: const Text('Answer 4'),
+        ),
+      ],
+    );
   }
 }
